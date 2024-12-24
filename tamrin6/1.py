50d8e4f7787a387f926de74a2f6535a30498702e
Shayan Shahbazi <upm200@gmail.com>
2024-12-24 13:44:48 +0330

    tamrin6

---
+++ tamrin6/1.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__(self):
+        pass
+
+    def deposit(self, amount):
+        pass
+
+    def withdraw(self, amount):
+        pass
+
+    def get_balance(self):
+        pass
+    def display_info(self):
+        pass
+
+# Using the BankAccount class
+
