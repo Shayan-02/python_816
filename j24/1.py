2673c285eb56cfdecc7edfda01d51f24eb2c1d31
Shayan Shahbazi <upm200@gmail.com>
2025-02-13 00:43:12 +0330

    j24

---
+++ j24/1.py
@@ -0,0 +1,15 @@
+import random as r
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+lst = [True, False, 1]
+a = np.array(lst)
+print(f"list: {lst}")
+print(f"array: {a}")
+
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(arr)
+
+lst2 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(lst2)
